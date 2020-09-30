f21105a64d64c1e57a91b34c3ede53e089c37078
Fahad Patel <42268318+fahad1403@users.noreply.github.com>
2020-10-01 00:28:01 +0530

    Update subtracting.py

--- subtracting.py
+++ subtracting.py
@@ -1,4 +1,9 @@
+#Subtraction of a number in Python code
 print("**** Subtract number ****")
+
+#take user input
 x = int(input("Enter first number"))
 y = int(input("Enter second number"))
+
+#Print the Output of x-y
 print("{} - {} = {}".format(x,y,(x-y)))
